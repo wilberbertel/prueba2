3236b9ffed568c964e1b567b310fd3763c03cbd2
1102883246 <1102883246@SJLBA0111B16.admcecar.local>
2019-03-21 07:50:18 -0500

    ecuacion archivo archivo javascrit

---
+++ index/public_html/archivo.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var t ="";
+t= prompt("ingrese su nombre");
+
+window.alert("tu nombre es : " + t);
